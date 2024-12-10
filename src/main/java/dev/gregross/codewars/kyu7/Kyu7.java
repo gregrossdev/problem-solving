eb4de46f6f90079710e891a9a637ff546c8cb688
Greg Ross <gregross.dev@gmail.com>
2024-12-09 22:29:21 -0500

    https://www.codewars.com/kata/52fba66badcd10859f00097e/solutions/java?filter=me&sort=best_practice&invalids=false

--- src/main/java/dev/gregross/codewars/kyu7/Kyu7.java
+++ src/main/java/dev/gregross/codewars/kyu7/Kyu7.java
@@ -3,7 +3,7 @@ package dev.gregross.codewars.kyu7;
 public class Kyu7 {
 
 	// https://www.codewars.com/kata/54ff3102c1bad923760001f3/solutions/java?filter=me&sort=best_practice&invalids=false
-	public static int getVowelCount(String str) {
+	public int getVowelCount(String str) {
 		int count = 0;
 		for (char ch: str.toCharArray()) {
 			if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
@@ -11,5 +11,15 @@ public class Kyu7 {
 			}
 		}
 		return count;
+	}
+
+	// https://www.codewars.com/kata/52fba66badcd10859f00097e/solutions/java?filter=me&sort=best_practice&invalids=false
+	public String disemvowel(String str) {
+		StringBuilder newStr = new StringBuilder();
+		str.chars()
+			.filter(ch -> "aeiouAEIOU".indexOf(ch) == -1)
+			.forEach(ch -> newStr.append((char) ch));
+
+		return newStr.toString();
 	}
 }
--- src/test/java/dev/gregross/codewars/kyu7/Kyu7Test.java
+++ src/test/java/dev/gregross/codewars/kyu7/Kyu7Test.java
@@ -6,12 +6,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class Kyu7Test {
 
+	Kyu7 kyu7 = new Kyu7();
+
 	@Test
 	void getVowelCountTests() {
-		assertEquals(5, Kyu7.getVowelCount("abracadabra"));
-		assertEquals(0, Kyu7.getVowelCount("bcdfg"));
-		assertEquals(3, Kyu7.getVowelCount("aei"));
-		assertEquals(3, Kyu7.getVowelCount("hello world"));
-		assertEquals(0, Kyu7.getVowelCount(""));
+		assertEquals(5, kyu7.getVowelCount("abracadabra"));
+		assertEquals(0, kyu7.getVowelCount("bcdfg"));
+		assertEquals(3, kyu7.getVowelCount("aei"));
+		assertEquals(3, kyu7.getVowelCount("hello world"));
+		assertEquals(0, kyu7.getVowelCount(""));
+	}
+
+	@Test
+	void disemvowelTests() {
+		assertEquals("brcdbr", kyu7.disemvowel("abracadabra"));
+		assertEquals("bcdfg", kyu7.disemvowel("bcdfg"));
+		assertEquals("", kyu7.disemvowel("aeiou"));
+		assertEquals("hll wrld", kyu7.disemvowel("hello world"));
+		assertEquals("", kyu7.disemvowel(""));
 	}
 }
