8672fb7157e4c62b56b8a2732d365a76c82e69d8
Greg Ross <gregross.dev@gmail.com>
2024-12-09 21:01:55 -0500

    https://www.hackerrank.com/challenges/simple-array-sum/problem?isFullScreen=true

--- src/main/java/dev/gregross/hackerrank/problem_solving/basic/Warmup.java
+++ src/main/java/dev/gregross/hackerrank/problem_solving/basic/Warmup.java
@@ -1,9 +1,19 @@
 package dev.gregross.hackerrank.problem_solving.basic;
 
+import java.util.List;
+
 public class Warmup {
 
 	// https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=true
 	public int solveMeFirst(int a, int b) {
 		return a + b;
+	}
+
+	// https://www.hackerrank.com/challenges/simple-array-sum/problem?isFullScreen=true
+	public int simpleArraySum(List<Integer> ar) {
+		int sum = 0;
+		for (Integer num : ar) sum += num;
+
+		return sum;
 	}
 }
--- src/test/java/dev/gregross/hackerrank/problem_solving/basic/WarmupTest.java
+++ src/test/java/dev/gregross/hackerrank/problem_solving/basic/WarmupTest.java
@@ -2,16 +2,27 @@ package dev.gregross.hackerrank.problem_solving.basic;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class WarmupTest {
 
+	Warmup warmup = new Warmup();
+
 	@Test
 	void solveMeFirstTest() {
-		Warmup warmup = new Warmup();
 		assertEquals(5, warmup.solveMeFirst(2, 3));
 		assertEquals(0, warmup.solveMeFirst(0, 0));
 		assertEquals(-1, warmup.solveMeFirst(-2, 1));
 		assertEquals(100, warmup.solveMeFirst(50, 50));
+	}
+
+	@Test
+	void simpleArraySumTest() {
+		assertEquals(6, warmup.simpleArraySum(Arrays.asList(1, 2, 3)));
+		assertEquals(0, warmup.simpleArraySum(Arrays.asList()));
+		assertEquals(-6, warmup.simpleArraySum(Arrays.asList(-1, -2, -3)));
+		assertEquals(15, warmup.simpleArraySum(Arrays.asList(1, 2, 3, 4, 5)));
 	}
 }
